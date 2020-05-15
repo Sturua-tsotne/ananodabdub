d3eb8ed8d9131364e76908d01e37e4969ada4c4c
Sturua-tsotne <47318145+Sturua-tsotne@users.noreply.github.com>
2020-05-16 00:33:17 +0400

    2

---
+++ qeifi.js
@@ -0,0 +1,48 @@
+// $(".zura").mouseenter(function () {
+//     $(".zura .text").css("display", "block");
+// });
+// $(".zura").mouseleave(function () {
+//     $(".zura .text").css("display", "none");
+// });
+
+// $(".anano").mouseenter(function () {
+//     $(".anano .text").css("display", "block");
+// });
+// $(".anano").mouseleave(function () {
+//     $(".anano .text").css("display", "none");
+// });
+
+// $(".TamTa").mouseenter(function () {
+//     $(".TamTa .text").css("display", "block");
+// });
+// $(".TamTa").mouseleave(function () {
+//     $(".TamTa .text").css("display", "none");
+// });
+
+// $(".qetia").mouseenter(function () {
+//     $(".qetia .text").css("display", "block");
+// });
+// $(".qetia").mouseleave(function () {
+//     $(".qetia .text").css("display", "none");
+// });
+
+// $(".mari").mouseenter(function () {
+//     $(".mari .text").css("display", "block");
+// });
+// $(".mari").mouseleave(function () {
+//     $(".mari .text").css("display", "none");
+// });
+
+// $(".dato").mouseenter(function () {
+//     $(".dato .text").css("display", "block");
+// });
+// $(".dato").mouseleave(function () {
+//     $(".dato .text").css("display", "none");
+// });
+
+// $(".temur").mouseenter(function () {
+//     $(".temur .text").css("display", "block");
+// });
+// $(".temur").mouseleave(function () {
+//     $(".temur .text").css("display", "none");
+// });
